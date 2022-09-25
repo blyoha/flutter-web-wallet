30f019ad02ed997af3ca28224ed552d39bd369e9
blyoha <filling_box@yahoo.com>
2022-09-26 02:51:30 +0300

    Added registration BLoC.

---
+++ lib/blocs/registration_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:web_wallet/blocs/registration_event.dart';
+import 'package:web_wallet/blocs/registration_states.dart';
+
+class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
+  RegistrationBloc() : super(RegistrationInitialState()) {
+    on<CodeSentEvent>((event, emit) => null);
+
+    on<ConfirmedEvent>((event, emit) => null);
+  }
+}

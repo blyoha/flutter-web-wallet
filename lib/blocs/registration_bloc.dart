e1ce9ebd88f030f572b89d9500166060e8a9124d
blyoha <filling_box@yahoo.com>
2022-09-26 04:14:27 +0300

    Fixed state emitting.

--- lib/blocs/registration_bloc.dart
+++ lib/blocs/registration_bloc.dart
@@ -4,7 +4,9 @@ import 'package:web_wallet/blocs/registration_states.dart';
 
 class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
   RegistrationBloc() : super(RegistrationInitialState()) {
-    on<CodeSentEvent>((event, emit) => null);
+    on<CodeSentEvent>((event, emit) {
+      emit(RegistrationCodeSentState());
+    });
 
     on<ConfirmedEvent>((event, emit) => null);
   }

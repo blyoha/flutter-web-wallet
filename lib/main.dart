7294e5a94df791c6669d718fed838235ca681fa9
blyoha <filling_box@yahoo.com>
2022-10-01 23:48:32 +0300

    Added Firebase dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:web_wallet/ui/pages/registration_page.dart';
 
 import 'constants/constants.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // Connect to Firebase app
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const WebMoneyApp());
 }
 
